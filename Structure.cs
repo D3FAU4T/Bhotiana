62283fd526abfcc589f16499ed3511a80d66a585
D3FAU4T <d3fau4t@gmail.com>
2024-06-08 18:32:09 +0530

    Added Structure for YAML and JSON

---
+++ Structure.cs
@@ -0,0 +1,19 @@
+﻿namespace Bhotiana
+{
+    public class YAMLSettings
+    {
+        public string Prefix { get; set; }
+        public string[] ViewersToIgnore { get; set; }
+        public Command[] Commands { get; set; }
+    }
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public string Response { get; set; }
+        public string Condition { get; set; }
+        public bool IsReply { get; set; }
+        public string ResponseConditional { get; set; }
+        public bool IsConditionalReply { get; set; }
+    }
+}
